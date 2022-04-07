8c3071683488f195c7bf8feec419b4c77a3d20ea
jbettmann <jordan.bettmann@gmail.com>
2022-04-06 18:12:30 -0600

    added function to handle numberOfEvents change

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    errorText: ''
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({
-      numberOfEvents: value
-    });
-  }
+    if (value <= 0 || value > 32) {
+      this.setState({
+        numberOfEvents: '',
+        errorText: 'Please enter a number between 1 and 32',
+      })
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: ""
+      });
+    }
+    this.props.updateNumberOfEvents(value);
+  };
 
 render () {
   return (
     <div className='numberOfEvents'>
+      <div className='numberOfEventsAlert'>
+        <p>{this.state.errorText}</p>
+      </div>
       <p>Number of Events</p>
       <input 
       type='number'
