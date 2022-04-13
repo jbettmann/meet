e4d51e74c5a430a27da98f22ceeb2a9c1593087f
jbettmann <jordan.bettmann@gmail.com>
2022-04-12 21:46:28 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -55,7 +55,6 @@ export const getEvents = async () => {
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    console.log('offline:', data);
     return data ? JSON.parse(data).events:[];
     
   }
@@ -69,7 +68,7 @@ export const getEvents = async () => {
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data.events));
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
     }
     NProgress.done();
